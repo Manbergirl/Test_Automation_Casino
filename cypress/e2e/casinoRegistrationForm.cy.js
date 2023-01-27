d8c5bc974ff1a67fea4474b38dbe9f4b6ed73dca
Beatris Dimitrova <Beatris.Dimitrovaaa@gmail.com>
2023-01-27 11:15:03 +0200

    test:Verify that the user can successfully add FirstName

---
+++ cypress/e2e/casinoRegistrationForm.cy.js
@@ -0,0 +1,22 @@
+describe('Verify that the user can successfully register an account', () => {
+    beforeEach(() => {
+      cy.visit(Cypress.config().baseUrl)
+    });
+
+    it('Verify that the user can successfully add FirstName:', () => {
+  
+        cy.visit("https://winbet.bg/registration")
+        cy.get(':nth-child(1) > .form-group > #input-wrapper > .input-group > .form-control').type('Beatris')
+       
+    })
+
+    
+    // cy.get('.form-select__toggler .form-select__toggler-icon').select('5')
+
+    // it('Verify that the user can successfully add SecondName:', () => {
+  
+    //     cy.visit("https://winbet.bg/registration")
+    //     cy.get(':nth-child(2) > .form-group > #input-wrapper > .input-group > .form-control').type('Dimitrova')       
+    // })
+
+})
